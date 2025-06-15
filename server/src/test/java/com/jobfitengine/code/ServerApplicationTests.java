83be5a84ea343c575f50057f1c352be09a4e3df9
tlb-lemrabott <lemrabott.toulba@yahoo.com>
2025-06-14 21:13:14 -0400

    Submit spring starter project for the backend processing.

---
+++ server/src/test/java/com/jobfitengine/code/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jobfitengine.code;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
